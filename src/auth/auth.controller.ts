49f38a0ced2c869d0afefa0154c5e267879f15d2
umask22 <22bazanjonathan@gmail.com>
2026-02-22 23:07:43 -0300

    feat: hacemos un login del user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -15,8 +15,7 @@ export class AuthController {
 
   @MessagePattern('auth.login.user')
   loginUser(@Payload() loginUserDto: LoginUserDto) {
-    //return this.authService.loginUser(data.email, data.password);
-    return loginUserDto;
+    return this.authService.loginUser(loginUserDto);
   }
 
   @MessagePattern('auth.verify.user')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
 import { PrismaClient } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
-import { RegisterUserDto } from './dto';
+import { LoginUserDto, RegisterUserDto } from './dto';
 
 @Injectable()
 export class AuthService extends PrismaClient implements OnModuleInit {
@@ -38,6 +38,46 @@ export class AuthService extends PrismaClient implements OnModuleInit {
 
         //devolvemos todos los datos del usuario excepto la contraseña
         const {password: hashedPassword, ...rest} = newUser;
+
+        return {
+          user: rest,
+          token: 'ABC'
+        };
+      } catch (error) {
+        throw new RpcException({
+          status: 400,
+          message: error.message,
+        })
+      }
+    }
+
+    async loginUser(loginUserDto: LoginUserDto) {
+      const { email, password } = loginUserDto;
+    
+      try {
+        const user = await this.user.findUnique({
+          where: { email },
+        });
+
+        if(!user){
+          throw new RpcException({
+            status: 400,
+            message: 'User/Password not valid',
+          })
+        }
+
+        //comparamos la contraseña ingresada con el hash de la contraseña almacenada en la base de datos
+        const isPasswordValid = bcrypt.compareSync(password, user.password);
+
+        if(!isPasswordValid){
+          throw new RpcException({
+            status: 400,
+            message: 'Password/User not valid',
+          })
+        }
+
+        //devolvemos todos los datos del usuario excepto la contraseña
+        const {password: hashedPassword, ...rest} = user;
 
         return {
           user: rest,
